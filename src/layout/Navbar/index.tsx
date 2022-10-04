0b55de24b1a7d7973fc199baed786e4e7636006f
Faris Ashai <farisashai@gmail.com>
2022-10-04 13:39:58 -0700

    Implement static navbar

---
+++ src/layout/Navbar/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import ACMLogo from '@/assets/graphics/logo.png';
+import style from './style.module.scss';
+
+const Navbar = () => {
+  return (
+    <div className={style.container}>
+      <nav className={style.navbar}>
+        <Link href="/" passHref>
+          <a href="replace" className={style.navLeft}>
+            <Image
+              src={ACMLogo.src}
+              width={48}
+              height={48}
+              alt="ACM Navbar Logo"
+              className={style.logo}
+            />
+            <span className={style.title}>Membership Portal</span>
+          </a>
+        </Link>
+      </nav>
+      <div className={style.wainbow} />
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import Navbar from '@/layout/Navbar';
+import '@/styles/reset.scss';
+
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <Navbar />
+      <Component {...pageProps} />;
+    </main>
+  );
 }
