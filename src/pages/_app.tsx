04d7803d46b403f18fcc235657940b9e9902eef3
Faris Ashai <farisashai@gmail.com>
2022-10-07 04:13:42 -0700

    Fix CSS font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import Navbar from '@/layout/Navbar';
 import '@/styles/reset.scss';
 import '@/styles/vars.scss';
+
+import Navbar from '@/layout/Navbar';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
