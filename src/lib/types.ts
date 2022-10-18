edd941935f7e0819bc1a8bbea981ed031ea9c509
Faris Ashai <farisashai@gmail.com>
2022-10-18 05:00:39 -0700

    Fix Eslint warnings and add some basic types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,21 @@
-/* eslint-disable no-unused-vars */
+export enum UserAccessType {
+  RESTRICTED = 'RESTRICTED',
+  STANDARD = 'STANDARD',
+  STAFF = 'STAFF',
+  ADMIN = 'ADMIN',
+  MARKETING = 'MARKETING',
+  MERCH_STORE_MANAGER = 'MERCH_STORE_MANAGER',
+  MERCH_STORE_DISTRIBUTOR = 'MERCH_STORE_DISTRIBUTOR',
+}
+
+export enum UserAccountState {
+  PENDING = 'PENDING',
+  ACTIVE = 'ACTIVE',
+  BLOCKED = 'BLOCKED',
+  PASSWORD_RESET = 'PASSWORD_RESET',
+}
+
+export type Uuid = string;
 export interface LoginFormData {
   email: string;
   password: string;
@@ -15,4 +32,19 @@ export interface LoginFormProps {
   setEmail: (value: string) => void;
   setPassword: (value: string) => void;
   submitForm: () => any;
+}
+
+export interface LoggedInUser {
+  uuid: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  profilePicture: string | null;
+  accessType: UserAccessType;
+  state: UserAccountState;
+  graduationYear: number;
+  major: string;
+  bio: string | null;
+  points: number;
+  credits: number;
 }
